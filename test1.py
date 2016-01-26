852100c098eb1e2138376efc3369317f4f3395f2
Michael Mäder <mike@moik.org>
2016-01-26 16:43:25 +0100

    added test1.py

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@
 import requests
 from sys import exit
 import pprint
-import ConfigParser
+import configparser
 
 
 def ConfigSectionMap(section):
@@ -21,23 +21,25 @@ def ConfigSectionMap(section):
 
 
 # get the config data from the config file
-config = ConfigParser.ConfigParser()
+config = configparser.ConfigParser()
 config.read('./config.rc')
 
 uname = ConfigSectionMap("Credentials")['username']
 passwd = ConfigSectionMap("Credentials")['password']
 myStrom_url = ConfigSectionMap("MyStrom")['baseurl']
+# TODO: test if this is in the PyCharm Todo list
+
 
 
 auth_url = myStrom_url + 'auth?email={eMail}&password={password}'.format(eMail=uname, password=passwd)
 r = requests.get(auth_url)
 if r.status_code != 200:
-    print "Error, no connection and authentication possible"
+    print ("Error, no connection and authentication possible")
     exit(1)
 
 r_json= r.json()
 if r_json['status'] != 'ok':
-    print "Authentication failed"
+    print ("Authentication failed")
     exit(1)
 
 # save the authorization token for all coming requests
@@ -47,22 +49,22 @@ authToken = r_json['authToken']
 url_getStatus = myStrom_url + '{method}?authToken={token}'.format(method='devices', token=authToken)
 r = requests.get(url_getStatus)
 if r.json()['status'] != 'ok':
-    print "Error getting the list"
+    print ("Error getting the list")
     exit(1)
 
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(r.json())
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(r.json())
 
 for myobject in r.json()['devices']:
     if myobject['name'] == 'Raspberry Pi':
-        print myobject
+        print (myobject)
 
 # get the weather
 url_getStatus = myStrom_url + '{method}?authToken={token}'.format(method='weather', token=authToken)
 r = requests.get(url_getStatus)
 if r.json()['status'] != 'ok':
-    print "Error getting the list"
+    print ("Error getting the list")
     exit(1)
-print r.json()
+print (r.json())
 
 
